6a68126a1dbe43df7cc8a6d4d023ae40b3c49e5d
markus_lu <markus.luckau@gmail.com>
2020-11-10 18:29:07 +0100

    operatoren

---
+++ operatoren.lua
@@ -0,0 +1,88 @@
+
+--- Arytmetische Operatoren
+
+a=2
+b=2
+-- +
+-- Adds two operands
+c=a+b
+
+-- -
+-- subtracs two operands
+c=a-b
+
+-- *
+-- Multiply two operands
+c=a*b
+
+-- /
+-- divide two operands
+c=a/b
+
+-- %
+-- Modulus Operator and remainder of after an integer division
+c=a%b
+
+-- ^
+-- Exponent Operator takes the exponents
+c=a^b
+
+
+ --- Relational Operators
+-- c enthält einen boolien (Wahr /falsch)
+-- Diese operatorn dienen zur Bedingungscheckung
+
+-- ==
+-- Vergleicht ob Zwei Operatoren gleich sind
+c = a==b
+
+-- ~=
+-- Vergleicht ob Zwei Operatoren ungleich sind
+c = a~=b
+
+-- >
+-- Vergleicht ob der linke operant größer als der rechte ist sind
+c = a>b
+
+-- <
+-- Vergleicht ob der linke operant kleiner als der rechte ist sind
+c = a<b
+
+-- >=
+-- Vergleicht ob der linke operant größer oder gleich als der rechte ist sind
+c = a>=b
+
+-- <=
+-- Vergleicht ob der linke operant kleiner oder gleich als der rechte ist sind
+c = a<=b
+
+--- Logische Operatoren
+-- logische operatoren dienen zur verklüofung von Variablen
+
+-- and
+-- und Verküpfung
+c = a and b
+
+-- or
+-- oder Verküpfung
+c = a or b
+
+-- not
+-- negation
+c = not a
+
+--- Miscellaneous operators
+
+-- ..
+-- zur Zusammenführung von zwei Strings
+-- f ist gleich "hallo Raspi64
+d= hallo
+e= Raspi64
+f = d..e
+
+-- #
+-- giebt die länge eines Strings aus
+-- f ist gleich 5
+f= #a
+
+
